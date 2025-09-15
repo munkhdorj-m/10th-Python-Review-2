bafb370eedbb33c822d40d132619fc3f5191ff26
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-15 23:09:41 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,26 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def rectangle_area(length, width):
+def even_numbers():
     # Write your code here
     pass
 
 # Exercise 2
-def check_number(num):
+def reverse_number(num):
     # Write your code here
     pass
 
 # Exercise 3
-def is_divisible_by_5(num):
+def sum_to_n(num):
     # Write your code here
     pass
 
 # Exercise 4
-def sum_is_even_or_odd(a, b, c, d):
+def is_prime(num):
     # Write your code here
     pass
 
 # Exercise 5
-def divisible_by_sum_of_digits(num):
+def perfect_squares():
     # Write your code here
     pass
